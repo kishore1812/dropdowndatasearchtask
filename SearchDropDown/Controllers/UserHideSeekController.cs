07faf145fd96405211ed6db7c183255c00b3643f
kishor <kishor@KISHOR-PC>
2019-09-15 10:04:49 +0530

    Add project files.

---
+++ SearchDropDown/Controllers/UserHideSeekController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchDropDown.Models;
+
+namespace SearchDropDown.Controllers
+{
+    public class UserHideSeekController : Controller
+    {
+        DropDown1Entities db = new DropDown1Entities();
+        // GET: UserHideSeek
+        public ActionResult Index()
+        {
+            ViewBag.UserData = new SelectList(db.tblUsers, "userId", "UserName");
+            return View();
+        }
+        public JsonResult getUserDetails(int id)
+        {
+            return Json(db.tblUsers.Find(id), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
